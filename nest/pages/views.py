4ea0393831e1f764391849b73764091aa3149adb
abolfazlparadox <104286799+Abolfazlparadox@users.noreply.github.com>
2024-02-26 17:45:46 +0330

    delete all

--- nest/pages/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,get_object_or_404
-from .models import PagesList
-# Create your views here.
-def pages(request , slug):
-    page = get_object_or_404(PagesList, slug=slug, active=True)
-    context = {
-        "page": page,
-        }
-    return render(request , 'pages/pages.html' , context)
--- nest/supplier_panel/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SupplierPanelConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'supplier_panel'
